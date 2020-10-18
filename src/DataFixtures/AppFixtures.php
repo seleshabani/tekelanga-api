368ff205c59d70ee06fe05463d1962fab296510b
seleshabani <seleshabani4@gmail.com>
2020-10-18 13:43:16 +0100

    first fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,7 +4,7 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-
+use Faker\Factory;
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
